42ad461f12c03e5ebfa1973a2098be567b5ccf69
tomfun <tomfun1990@gmail.com>
2016-01-14 20:51:43 +0200

    development bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            new Brander\Bundle\EAVBundle\BranderEAVBundle(),
+            new Werkint\Bundle\StatsBundle\WerkintStatsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
